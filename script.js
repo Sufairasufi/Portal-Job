b29bcc822d4079c990d59bdf9360a30574405851
Sufairasufi <ashrafsufaira@gmail.com>
2025-05-23 12:27:40 +0530

    changes

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
 document.getElementById('jobForm').addEventListener('submit', function (e) {
   e.preventDefault();
 
-  const name = document.getElementById('name').value.trim();
-  const email = document.getElementById('email').value.trim();
-  const position = document.getElementById('position').value;
-  const resume = document.getElementById('resume').files[0];
-  const message = document.getElementById('message');
+  var name = document.getElementById('name').value.trim();
+  var email = document.getElementById('email').value.trim();
+  var position = document.getElementById('position').value;
+  var resume = document.getElementById('resume');
 
-  if (name && email && position && resume) {
-    message.textContent = "✅ Application submitted successfully!";
-    message.className = "text-success";
-    // message.scrollIntoView({ behavior: "smooth", block: "center" });
+  var nameError = document.getElementById("nameerror")
+  var emailError = document.getElementById("emailerror")
+  var positionError = document.getElementById("positionerror")
+  var resumeError = document.getElementById("resumeerror")
 
-    this.reset();
+  nameError.textContent="";
+  emailError.textContent="";
+  positionError.textContent="";
+  resumeError.textContent="";
+  var isValid=true;
 
-    setTimeout(() => {
-      message.textContent = "";
-      message.className = "";
-      message.style.display = "none";
-    }, 3000);
+  if(name === ""){
+    nameError.textContent = "Must have a name";
+    isValid=false;
+  }
 
-    // Message visible cheyyanam
-    message.style.display = "block";
-  } else {
-    message.textContent = "❌ Please complete all fields.";
-    message.className = "text-danger";
-
-    setTimeout(() => {
-      message.textContent = "";
-      message.className = "";
-      message.style.display = "none";
-    }, 3000);
-
+  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if(email === "") {
+    emailError.textContent = "Email is required";
+    isValid=false;
+  }else if (!emailPattern.test(email)){
+    emailError.textContent="Invalid email format"
+    isValid = false;
+  }
   
-    message.style.display = "block";
-}
+  if(position === ""){
+    positionError.textContent = "Must have a position";
+  }
+
+  if(resume.files.length === 0){
+    resumeError.textContent = "Must have a resume";
+        isValid = false;
+
+  }
+  if (!isValid){
+    return;
+  }
+  alert("✅ Application submitted successfully!");
+
+  document.getElementById("jobform").reset();
 });
+
+function sendEmail(event){
+  event.preventDefault();
+  const details = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    position: document.getElementById("position").value,
+    resume: document.getElementById("resume").value
+  }
+  emailjs.send("service_38aigsl","template_gfzgl9e",details)
+  .then(function(response){
+    document.getElementById("jobForm").reset();
+  },function(error) {
+  });
+}
+
 
 document.getElementById('contact-form').addEventListener('submit', function (e) {
   e.preventDefault();
@@ -44,13 +71,57 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
   const email = document.getElementById('contact-email').value.trim();
   const message = document.getElementById('contact-message').value.trim();
 
-  if (name && email && message) {
-    alert(`Thanks, ${name}! Your message has been sent.`)
-    this.reset();
-  }else {
-    alert("Please fill in all fields.")
+  var contactNameError = document.getElementById("contactnameerror");
+  var contactEmailError = document.getElementById("contactemailerror");
+  var contactMessageError = document.getElementById("messageerror");
+
+  contactNameError.textContent="";
+  contactEmailError.textContent="";
+  contactMessageError.textContent="";
+  var isValid=true;
+
+  if(name === ""){
+    contactNameError.textContent = "Must have a name";
+    isValid=false;
   }
+
+  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if(email === "") {
+    contactEmailError.textContent = "Email is required";
+    isValid=false;
+  }else if (!emailPattern.test(email)){
+    contactEmailError.textContent="Invalid email format"
+    isValid = false;
+  }
+
+  if(message === ""){
+    contactMessageError.textContent = "Must have a message";
+    isValid=false;
+  }
+
+  if (!isValid){
+    return;
+  }
+
+  alert("✅ Message sended successfully!");
+
+  document.getElementById("contact-form").reset();
 });
+
 document.getElementById('menu-icon').addEventListener('click', function () {
   document.getElementById('navbar').classList.toggle('active');
 });
+
+function sendContact(event){
+  event.preventDefault();
+  const contacts = {
+    name: document.getElementById("contact-name").value,
+    email: document.getElementById("contact-email").value,
+    message: document.getElementById("contact-message").value
+  }
+  emailjs.send("service_38aigsl","template_drhp3ts",contacts)
+  .then(function(response){
+    document.getElementById("contact-form").reset();
+  },function(error) {
+  });
+}
